70dbdcfdcce1509721a892c3604124732ba7a4e3
Gorky Kirill <59337566+ToxicSnail@users.noreply.github.com>
2024-11-22 18:27:12 +0700

    Update main.py

--- sql_injection_fixer/main.py
+++ sql_injection_fixer/main.py
@@ -1,19 +1,17 @@
-# sql_injection_fixer/main.py
+# main.py
 
 from analyzer import find_sql_injections
 from fixers import fix_sql_injections
 
 def main():
-    target_path = './test_code'  
+    target_path = '.'  # Убедись, что это правильно указывает на директорию с example.py
 
-    # Шаг 1: Анализ кода на наличие SQL-инъекций
     vulnerabilities = find_sql_injections(target_path)
 
     if vulnerabilities:
         print("Найдены потенциальные уязвимости:")
         for vuln in vulnerabilities:
             print(f"Файл: {vuln['file']}, Строка: {vuln['lineno']}")
-        # Шаг 2: Автофиксация уязвимостей
         fix_sql_injections(vulnerabilities)
         print("Уязвимости исправлены.")
     else:
