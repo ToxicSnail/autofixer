483b4f1f8bd86106eeb05c052102d5567f93a5fd
ToxicSnail <gorkykirill@stud.tsu.ru>
2025-03-16 19:35:36 +0700

    Введено то, что сейчас мы можем использовать в активном окружении через отдельные команды, не прыгая по поддиректориям, раньше это было пустой функцией, но теперь реализована

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        # Зависимости
+        'libcst'
     ],
     entry_points={
         'console_scripts': [
-            'sql-fix=sql_injection_fixer.main:main',
+            'sql-fix=sql_injection_fixer_v2.sql_fixer:main',
+            'eval-fix=eval_fixer.eval_fixer:main',
+            'test-sql=sql_injection_fixer_v2.test_sql_fixer:main',
         ],
     },
 )
