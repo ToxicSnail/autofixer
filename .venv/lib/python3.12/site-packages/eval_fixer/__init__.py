c3a98d8220229fb80b7dbc03cf3bba01567768b9
ToxicSnail <gorkykirill@stud.tsu.ru>
2025-04-18 18:32:18 +0700

    Stop tracking .venv and add it to .gitignore

--- .venv/lib/python3.12/site-packages/eval_fixer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package: eval_fixer
-Description: Contains modules for detecting and replacing eval() calls with safer alternatives.
-"""
-
-# Если нужно экспортировать какие-то объекты, сделайте импорт здесь.
-# from .eval_fixer import main
--- .venv/lib/python3.12/site-packages/sql_injection_fixer_v2/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package: sql_injection_fixer_v2
-Description: Contains modules for detecting and fixing SQL injection vulnerabilities.
-"""
-
-# Если нужно экспортировать какие-то объекты на уровень пакета, можно сделать это здесь.
-# from .sql_fixer import main
